05d4f422123f50a4e4ea72b0e595e70b2019a712
Juliana Prieto <jprietoarcila@gmail.com>
2019-09-23 23:06:30 -0500

    Initial changes

---
+++ myapp/client/main.js
@@ -0,0 +1,56 @@
+import "./main.html";
+
+Template.profile.created = () => {
+  console.log("Created the profile template");
+};
+
+Template.profile.rendered = () => {
+  console.log("Rendered the profile template");
+};
+
+/*otros elementos del archivo*/
+
+Template.profile.helpers({
+  /*other helpers*/
+  exampleHelper: () => {
+    return "string returned by exampleHelper";
+  },
+
+  profileList: () => {
+    return [
+      {
+        name: "Juan Rodríguez",
+        age: 25
+      },
+      {
+        name: "María Gómez",
+        age: 30
+      },
+      {
+        name: "Esteban Martínez",
+        age: 15
+      },
+      {
+        name: "Luisa Sánchez",
+        age: 19
+      }
+    ];
+  },
+  passingData: (myString1, myString2) => {
+    console.log(`These are the strings ${myString1} ${myString2}`);
+  },
+  randomHelper: () => {
+    return Session.get("randomNumber");
+  },
+      
+  profilesCollection: () => {
+    return Profiles.find({});
+}
+});
+
+Template.profile.events({
+  "click button": (e, i) => {
+    console.log("Button clicked");
+    Session.set("randomNumber", Math.random(0, 99));
+  }
+});
